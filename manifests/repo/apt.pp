f7a5197cb1240404f8a7d37e5676b9c0e3e6c790
treydock <tdockendorf@osc.edu>
2022-05-06 10:53:35 -0400

    Ensure nodejs 14 repo is present for Debian/Ubuntu (#88)
    * Ensure nodejs 14 repo is present for Debian/Ubuntu
    
    * Avoid 2.0 repos, likely rebase issue
    
    * Skip failing tests for Ubuntu till proper packages exist

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -24,4 +24,15 @@ class openondemand::repo::apt {
       'source' => $openondemand::repo_gpgkey,
     }
   }
+
+  apt::source { 'nodesource':
+    ensure   => 'present',
+    location => 'https://deb.nodesource.com/node_14.x',
+    repos    => 'main',
+    release  => $facts['os']['distro']['codename'],
+    key      => {
+      'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+      'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+    }
+  }
 }
